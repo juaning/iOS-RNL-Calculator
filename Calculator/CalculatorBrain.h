3c3073a17509bde69d48c6a8ab5e494b2bf2adf1
juaning <ian.mignaco@gmail.com>
2013-05-20 00:11:56 +1000

    Added basic functionalities to RNL Calculator

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,14 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Juan Mignaco on 19/05/13.
+//  Copyright (c) 2013 Juan Mignaco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+- (void)pushOperand: (double)operand;
+- (double)performOperation: (NSString *)operation;
+@end
